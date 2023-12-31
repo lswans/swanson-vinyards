c68151b250bad86a8fe12c32084c0cf373148865
lswans <lucasswanson88@outlook.com>
2023-12-31 11:59:17 -0800

    first commit

---
+++ swanson-vinyards/src/App.js
@@ -0,0 +1,18 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className="App-main">
+        
+        {/* Add more components and content here */}
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ swanson-vinyards/src/Header.js
@@ -0,0 +1,14 @@
+// src/Header.js
+import React from 'react';
+import './App.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <h1>Swanson Vinyards</h1>
+      {/* Add additional header content if needed */}
+    </div>
+  );
+}
+
+export default Header;
